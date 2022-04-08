88c8fb5e70f1614fc93cf4d7622215e63385ae13
Agustin Bereciartua <bereciartua.agustin@gmail.com>
2022-04-07 21:05:32 -0400

    feat apidocs

--- app/main.py
+++ app/main.py
@@ -16,17 +16,90 @@ buda=BudaHMACAuth(apiKey, secretKey)
 
 app = Flask(__name__)
 @app.route('/')
-def get_user():
+def getIndex():
     return current_app.send_static_file('index.html')
     
 
+@app.route("/addOrder", methods=["POST"])
+def addOrder():
+    try:
+        market_id = 'btc-clp'
+        url = f'https://www.buda.com/api/v2/markets/{market_id}/orders'
+       # auth = BudaHMACAuth(api_key, secret)
+        response = requests.post(url, auth=buda, json={
+            'type': 'Bid',
+            'price_type': 'limit',
+            'limit': 1000000,
+            'amount': 0.05,
+        })
+        print(response.json())
+    except:
+        print("An exception occurred")
+    return response.json()
+
+@app.route("/balances")
+def balances():
+
+    try:
+        
+        # buda.get_nonce()
+        # buda.sign()
+        # buda.__call__()
+        #print(buda.api_key)
+        request = requests.get("https://www.buda.com/api/v2/balances", auth=buda)
+        if request.status_code == requests.codes.ok:
+            print(request.headers['content-type'])
+            print(request.json())
+            print("aca")
+        else:
+            print("no")
+    except:
+        print("An exception occurred")
+    
+    return request.json()
+
+@app.route("/orders")
+def orders():
+    try:
+        market_id = 'btc-clp'
+        url = f'https://www.buda.com/api/v2/markets/{market_id}/orders'
+        response = requests.get(url, auth=buda, params={
+            'state': 'pending',
+            'per': 20,
+            'page': 1,
+        })
+        print(response.json())
+    except:
+        print("An exception occurred")
+    
+    return response.json()
+
+@app.route("/withdrawals")
+def withdrawals():
+    try:
+        currency = 'btc'
+        url = f'https://www.buda.com/api/v2/currencies/{currency}/withdrawals'
+
+        response = requests.post(url, auth=buda, json={
+            'amount': 0.05,
+            'simulate': True,
+            'amount_includes_fee': True,
+            'withdrawal_data': {
+                'target_address': 'mo366JJaDU5B1hmnPygyjQVMbUKnBC7DsY',
+            },
+        })
+        print(response.json())
+    except:
+        print("An exception occurred")
+    
+    return response.json()
 
 """
-@api {get} /simulate Simula transaction a mi cuenta de Buda.com
+@api {get} /simulate Simula transaction via unarequest POST a mi cuenta de Buda.com
 @apiName simulate
 @apiGroup simulate
 
-@apiSuccess {Object} response La respuesta de https://www.buda.com/api/v2/currencies/clp/deposits
+@apiSuccess {Object} response La respuesta de POST https://www.buda.com/api/v2/currencies/clp/deposits
 
 """
 @app.route("/simulate", methods=["GET"])
@@ -60,39 +133,3 @@ def proofOfBuda():
     response=prueba.pown()
     return jsonify(response)
 
-@app.route("/addOrder", methods=["POST"])
-def addOrder():
-    try:
-        market_id = 'btc-clp'
-        url = f'https://www.buda.com/api/v2/markets/{market_id}/orders'
-       # auth = BudaHMACAuth(api_key, secret)
-        response = requests.post(url, auth=buda, json={
-            'type': 'Bid',
-            'price_type': 'limit',
-            'limit': 1000000,
-            'amount': 0.05,
-        })
-        print(response.json())
-    except:
-        print("An exception occurred")
-    return response.json()
-
-@app.route("/balances")
-def hello_world():
-    try:
-        
-        # buda.get_nonce()
-        # buda.sign()
-        # buda.__call__()
-        #print(buda.api_key)
-        request = requests.get("https://www.buda.com/api/v2/balances", auth=buda)
-        if request.status_code == requests.codes.ok:
-            print(request.headers['content-type'])
-            print(request.json())
-            print("aca")
-        else:
-            print("no")
-    except:
-        print("An exception occurred")
-    
-    return request.json()
